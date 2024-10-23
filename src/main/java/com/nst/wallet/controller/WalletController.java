7647c5380d5ca211fa1ac5fa13f90ca0bec717e8
Ashok Jaiswal <ashokjaiswal@Ashoks-MacBook-Pro.local>
2024-10-23 09:43:29 +0800

    * Added env variables

--- src/main/java/com/nst/wallet/controller/WalletController.java
+++ src/main/java/com/nst/wallet/controller/WalletController.java
@@ -1,5 +1,6 @@
 package com.nst.wallet.controller;
 
+import io.micronaut.context.annotation.Value;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
@@ -12,8 +13,15 @@ import org.slf4j.LoggerFactory;
 
 @Controller("/wallet") // Base URL for this controller
 public class WalletController {
-
+    @Value("${NEXT_PUBLIC_CIRCLE_API_KEY}")
+    private String circleApiKey;
+    
     private static final Logger LOG = LoggerFactory.getLogger(WalletController.class);
+
+    @Get("/circle-api-key")
+    public String getCircleApiKey() {
+        return circleApiKey; // Returns the Circle API Key
+    }
 
     @Post("/create") 
     public HttpResponse<String> createWallet(@Body WalletRequest request) {
@@ -22,7 +30,7 @@ public class WalletController {
         LOG.info("Creating wallet for user ID: {}", userId); // Log user ID
         // Implement your logic here (e.g., save to database)
         
-        return HttpResponse.created("Wallet created for user ID: " + userId);
+        return HttpResponse.created("Wallet created for user ID: "  + getCircleApiKey());
     }
     @Get("/") // New GET endpoint to return "Hello World"
     public String helloWorld() {
